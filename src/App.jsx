4b1ee59b861e0b57d4c9e86b516b011d852aa5f4
allsyalwaa <zulfiaalsyalwa@gmail.com>
2024-10-22 12:28:39 +0700

    Add new routes for Pages, Article, and SinglePost components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Pages from "./pages/Pages";
+import Article from "./pages/Article";
+import SinglePost from "./pages/SinglePost";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/pages" element={<Pages />} />
+        <Route path="/article" element={<Article />} />
+        <Route path="/single-post" element={<SinglePost />} />
       </Routes>
     </BrowserRouter>
   );
